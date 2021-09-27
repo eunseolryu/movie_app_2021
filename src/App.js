709200a1fb6340e737628f975739e7b56eda3ea5
seol <rrrreeees894@gmail.com>
2021-09-27 15:44:00 +0900

    movie app add

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';   // npm install axios로 다운 받고 import.
+import Movie from './Movie';
+import './App.css';
 
-function Food({ name, picture }) {
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture} />
-  </div>
-}
-
-const foodIlike = [
-  {
-    name: "Kimchi",
-    image:
-      "https://thumbnail10.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2020/12/01/21/0/e209d9b0-9ef0-4d66-ac04-59386c34def1.jpg"
-  },
-  {
-    name: "Samqyeopsal",
-    image:
-      "https://thumbnail10.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2020/12/01/21/0/e209d9b0-9ef0-4d66-ac04-59386c34def1.jpg"
-  },
-  {
-    name: "Bibimbap",
-    image:
-      "https://thumbnail10.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2020/12/01/21/0/e209d9b0-9ef0-4d66-ac04-59386c34def1.jpg"
-  },
-  {
-    name: "Doncasu",
-    image:
-      "https://thumbnail10.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2020/12/01/21/0/e209d9b0-9ef0-4d66-ac04-59386c34def1.jpg"
-  },
-  {
-    name: "Kimbap",
-    image:
-      "https://thumbnail10.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/2020/12/01/21/0/e209d9b0-9ef0-4d66-ac04-59386c34def1.jpg"
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {  //awati를 사용하려면 async와 함께.
+    const {
+      data: {
+        data: { movies }  //api에서 보면 data안에 data안에 movies가 있음.
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json")
+    this.setState({ movies, isLoading: false })  // <- ({movies:movies}) <- state의 movies와 axios의 movies를 줄여서 사용가능.
+  };
+  componentDidMount() {
+    this.getMovies();
   }
-]
-
-function App() {
-  return (
-    <div>
-      {foodIlike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}
-    </div>
-  );
+  render() {
+    const { isLoading, movies } = this.state;  //movies는 위 api에 있는 movies목록
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    )
+  }
 }
 
-export default App;
+  export default App;
